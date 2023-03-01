427aca218c0ba1eb104eb6a97a3df06e2b9b0fe0
saurav1812 <shorya181299@gmail.com>
2023-03-01 12:33:48 +0530

    new code

---
+++ Check_Palindrome.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+bool checkPalindrome(char str[]) {
+    // Write your code here
+    int count=0;
+    for(int i=0; str[i] !='\0'; i++){
+        count++;
+    }
+    int j=count-1;
+    for(int i=0;i<count/2;i++){
+          if(str[i]!=str[j]){
+             return false;
+          }
+       j--;
+      }
+    return true;
+}
+
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin >> str;
+    cout << (checkPalindrome(str) ? "true" : "false");
+}
