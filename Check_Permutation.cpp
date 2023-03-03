f6c627dcce9747c5002f757819f7d426e763b4be
saurav1812 <shorya181299@gmail.com>
2023-03-04 01:38:40 +0530

    intial changes

---
+++ Check_Permutation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+int main() {
+    int size = 1e6;
+    char str1[size];
+    char str2[size];
+    cin >> str1 >> str2;
+    cout << (isPermutation(str1, str2) ? "true" : "false");
+}
