f31362d6bcdb9cd6ad279bb4fae592d3120d60be
saurav1812 <shorya181299@gmail.com>
2023-03-04 01:39:05 +0530

    intail changes

--- Check_Permutation.cpp
+++ Check_Permutation.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+#include <bits/stdc++.h>
+bool isPermutation(char input1[], char input2[]) {
+    // Write your code here
+    string temp(input1);
+    string newtemp(input2);
 
+    int a=temp.length();
+    int b=newtemp.length();
+    if (a != b) {
+      return false;
+    }
+    sort( temp.begin() , temp.end() );
+    sort( newtemp.begin() , newtemp.end() );
+
+    for (int i = 0; i < a;  i++)
+      if (temp[i] != newtemp[i]) 
+        return false;
+      
+     
+        return true;
+      
+    
+}
 
 
 int main() {
