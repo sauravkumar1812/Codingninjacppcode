8fb5baded59f5906843d28df6afcaee0cee8cc99
saurav1812 <shorya181299@gmail.com>
2023-03-04 01:42:36 +0530

    intial changes

--- Remove_Consecutive_Duplicates.cpp
+++ Remove_Consecutive_Duplicates.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+#include<bits/stdc++.h>
+void removeConsecutiveDuplicates(char input[]) {
+    // Write your code here
+   int i = 0;
+  int j = 1;
+  int k = 0;
+  while(i<strlen(input)){
+    input[k]=input[i];
+    while(input[i]==input[j]){
+      i++;
+      j++;
+    }
+    i++;
+    j++;
+    k++;
+  }
+  input[k]='\0';
+  
 
+}
 
 
 int main() {
