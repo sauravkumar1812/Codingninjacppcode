d37c4b6bf149cdd9cdd186bdebac6581175174dd
saurav1812 <shorya181299@gmail.com>
2023-03-05 23:18:35 +0530

    intial changes

--- Reverse_Each_Word.cpp
+++ Reverse_Each_Word.cpp
@@ -2,6 +2,29 @@
 #include <cstring>
 using namespace std;
 
+#include<bits/stdc++.h>
+void reverseEachWord(char input[]) {
+ 
+int temp;
+string str(input);
+int start = 0,end = 0;
+for(int i=0;i<=strlen(input);i++){
+  // for(int j=1;input[j] != '\0';j++){
+    if(input[i] == ' ' || input[i] == '\0'){
+      end = i-1;
+    while(start<end){
+    char temp = input[start];
+    input[start] = input[end];
+    input[end] = temp;
+    start++;
+    end--;
+  }
+  start = i+1;
+}
+}
+
+
+}
 
 
 int main() {
