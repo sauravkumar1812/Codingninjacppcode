2b80e3a3ba1de244931397b44e5238e9f5e8c3e1
saurav1812 <shorya181299@gmail.com>
2023-03-05 23:23:59 +0530

    intial changes

--- Compress_the_String.cpp
+++ Compress_the_String.cpp
@@ -3,6 +3,35 @@
 #include<string>
 using namespace std;
 
+string getCompressedString(string &input) {
+   
+   string s ="";
+   int start =0, end =1,count=1;
+   int n = input.size();
+
+   while(start <= n && end <= n)
+   {
+      if(input[start] == input[end])
+      {
+          end++;
+          count++;
+      }
+      else
+      {
+          s += input[start];
+
+          if(count > 1)
+          {
+              s += to_string(count);
+          }
+          count = 1;
+          start = end;
+          end++; 
+      }
+   }
+
+   return s;
+}
 
 
 int main() {
