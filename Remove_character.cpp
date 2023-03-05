16e73a09337fcd381699bb14d400a59b4d7b0271
saurav1812 <shorya181299@gmail.com>
2023-03-05 23:20:23 +0530

    intial changes

--- Remove_character.cpp
+++ Remove_character.cpp
@@ -2,6 +2,17 @@
 #include <cstring>
 using namespace std;
 
+void removeAllOccurrencesOfChar(char input[], char c) {
+    // Write your code here
+   int j, n = strlen(input);
+    for (int i = j = 0; i < n; i++)
+        if (input[i] != c)
+            input[j++] = input[i];
+ 
+    input[j] = '\0';
+
+  
+}
 
 int main() {
     int size = 1e6;
