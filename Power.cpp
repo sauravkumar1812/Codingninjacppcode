7e35db653acb20dad07b9e80032ba032f3d336e5
saurav1812 <shorya181299@gmail.com>
2023-04-08 15:05:43 +0530

    final changes

--- Power.cpp
+++ Power.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
-#include "Solution.h"
-using namespace std;
 
+using namespace std;
+int power(int x, int n) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+
+if(n==0){
+  return 1; 
+}
+if(x==0){
+ return 0;
+}
+  return x * power(x,n-1);
+}
 
 int main(){
     int x, n;
