462c39209f05330010e101dda957131949538a3c
saurav1812 <shorya181299@gmail.com>
2023-04-08 15:06:59 +0530

    final changes

--- Print Numbers.cpp
+++ Print Numbers.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 
+void print(int n){
+    //write your code here
+   int i=1;
+   int ans;
+
+ while(i<=n){
+     ans=1*i;
+     cout<<ans<<" ";
+     i++;
+ }
+
+}
 
 int main(){
     int n;
