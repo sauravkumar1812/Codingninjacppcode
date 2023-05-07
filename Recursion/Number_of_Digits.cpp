d467846217b1884ea3728cf0f1bbb28ae06c7750
saurav1812 <shorya181299@gmail.com>
2023-05-07 16:22:46 +0530

    new code

---
+++ Recursion/Number_of_Digits.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+int count(int n){
+    //write your code here
+    // int sum=0;
+    if (n == 0) {
+      return 0;
+    }
+    
+      int smallout=1+count(n/10);
+
+  
+  return smallout;
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    cout << count(n) << endl;
+}
+
+
+
+
+
+
+
+
+
+
