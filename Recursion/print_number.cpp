9b3fbc4975c73abfa3f6c5c03b96b7f490be7265
saurav1812 <shorya181299@gmail.com>
2023-05-07 16:21:30 +0530

    new code

---
+++ Recursion/print_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    // write your code here
+    int i = 1;
+    int ans;
+
+    while (i <= n)
+    {
+        ans = 1 * i;
+        cout << ans << " ";
+        i++;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    print(n);
+}
