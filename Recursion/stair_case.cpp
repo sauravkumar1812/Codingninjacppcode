483349bebe8cff46d9c3f7be860f99b4b2ea3ba5
saurav1812 <shorya181299@gmail.com>
2023-05-07 16:49:36 +0530

    new code

--- Recursion/stair_case.cpp
+++ Recursion/stair_case.cpp
@@ -2,11 +2,7 @@
 #include "solution.h"
 using namespace std;
 int staircase(int n){
-    /* Don't write main().
-     *  Don't read input, it is passed as function argument.
-     *  Return output and don't print it.
-     *  Taking input and printing output is handled automatically.
-     */
+   
     
     if(n<0){
         return 0;
