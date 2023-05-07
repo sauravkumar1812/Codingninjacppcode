b913ebd42a4f537e34568d6b3bc3640f1bf457e5
saurav1812 <shorya181299@gmail.com>
2023-05-07 17:10:17 +0530

    new code

---
+++ DASTEST1/maximum_profit_on_app.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include "solution.h"
+
+#include<bits/stdc++.h>
+    int maximumProfit(int budget[], int n) {
+
+    int ans=INT_MIN;
+    sort(budget,budget+n);
+    for(int i=0;i<n;i++)
+    {
+        ans=max(ans,budget[i]*(n-i));
+    }
+    return ans;
+
+}
+
+
+int main() {
+    int n ,*input,i,*cost;
+    cin>>n;
+    input = new int[n];
+    for(i = 0;i < n;i++)
+        cin>>input[i];
+    
+    cout << maximumProfit(input, n) << endl;
+    
+}
